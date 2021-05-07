d7ac346b3f10f3b3781970c2ca607f76c8a991b4
Jonathan Alan de Moura <>
2021-05-07 10:14:53 -0300

    Subindo o gitHub

--- src/main/java/br/com/compasso/avaliacao/avaliacaobackend/AvaliacaoBackendApplication.java
+++ src/main/java/br/com/compasso/avaliacao/avaliacaobackend/AvaliacaoBackendApplication.java
@@ -8,6 +8,7 @@ import org.springframework.cache.annotation.EnableCaching;
 @EnableCaching
 public class AvaliacaoBackendApplication {
 
+    //
     public static void main(String[] args) {
         SpringApplication.run(AvaliacaoBackendApplication.class, args);
     }
